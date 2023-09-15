dcc5fcc5bc72feebbe6ae52ce905cda049ded9e4
MG-Belos <aladeangel07@gmail.com>
2023-09-14 18:28:35 -0600

    v3circulo

--- Figuras.cpp
+++ Figuras.cpp
@@ -1,14 +1,17 @@
 #include <stdio.h>
 int main() {
 	float fBase, fAltura, fArea;
-	printf("Calcular el Area de un Cuadrado \n");
 	printf("Calcular el Area de un Rectangulo \n");
 	printf("introduzca la cantidad para la Base: ");
 	scanf_s("%f", &fBase);
+	float fArea, fRadio;
+	printf("Calcular el Area de un Circulo \n");
+	printf("introduzca la cantidad para el radio: ");
+	scanf_s("%f", &fRadio);
 	printf("Introduzca la cantidad para la altura: ");
 	scanf_s("%f", &fAltura);
 	fArea = fBase * fAltura;
-	printf("El area para el cuadrado es: %f", fArea);
+	fArea = fRadio * fRadio * 3.1416;
 	printf("El area para el rectangulo es: %f", fArea);
 
 }
